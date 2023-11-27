b0ef7b8ba5f906f7b046061efbe42e6d9a86365f
Milan <gebeimilan@gmail.com>
2023-11-27 01:15:02 +0100

    Carousel megcsinálva + layout működik (componentsben van)

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Albérletközvetítő</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="{{asset('css\style.css')}}" type="text/css">
+</head>
+<body>
+<nav data-mdb-navbar-init class="navbar shadow-lg fixed-top navbar-expand-lg navbar-light bg-body-tertiary">
+    <!-- Container wrapper -->
+    <div class="container-fluid">
+        <!-- Collapsible wrapper -->
+        <div class="collapse navbar-collapse" id="navbarLeftAlignExample">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="navbar-brand me-2" href="/">
+                        <img src="{{asset('img\icon.png')}}" alt="Logo" width="50px" height="50px"/>
+                    </a>
+
+                </li>
+            </ul>
+        </div>
+
+        <div class="collapse navbar-collapse justify-content-center" id="navbarCenteredExample">
+            <!-- Left links -->
+            <ul class="navbar-nav mb-2 mb-lg-0 a">
+                <li class="nav-item">
+                    <a class="nav-link active fejlec" aria-current="page" href="/">Kezdőlap</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link fejlec" href="#">Ingatlanok</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link fejlec" href="#">Kollégáink</a>
+                </li>
+            </ul>
+            <!-- Left links -->
+        </div>
+
+        <div class="collapse navbar-collapse" id="navbarRightAlignExample">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link fejlec" href="{{ route('register') }}">Regisztráció</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link fejlec" href="{{ route('login') }}">Bejelentkezés</a>
+                </li>
+            </ul>
+        </div>
+        <!-- Collapsible wrapper -->
+    </div>
+    <!-- Container wrapper -->
+</nav>
+
+<main>
+    {{$slot}}
+</main>
+</body>
+</html>
