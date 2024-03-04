4dfdcc3577d75e626c4e616b759019ca4acbf2fa
Milan <gebeimilan@gmail.com>
2024-03-04 16:42:45 +0100

    Saját ingatlanok szépen megjelennek

--- resources/views/properties/own.blade.php
+++ resources/views/properties/own.blade.php
@@ -1,5 +1,38 @@
-@foreach($properties as $property)
-    <h1>{{$property->settlement}}</h1>
-
-@endforeach
-
+<x-layout>
+    <div class="container margin-top">
+        <div class="row">
+            @foreach($properties as $property)
+                @php
+                    $address = ($property->settlement).','.' '.($property->address).'.';
+                @endphp
+                {{--<iframe src="https://maps.google.it/maps?q=<?php echo $address?>&output=embed" width="600" height="450"
+                        style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>--}}
+                <div class="col-lg-3 width-33 mb-5">
+                    <div class="card border-0 shadow-2xl" style="width: 25rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h1 class="card-title">{{number_format(($property->price),0,'','.')}} Ft</h1>
+                            <p class="card-text mb-5">{{$address}}</p>
+                            <div class="row mb-4">
+                                <div class="col-md-3 width-33">
+                                    <div>
+                                        <i class="fa-solid fa-bed icon-size"></i><span class="px-2 font-weight-600">{{$property->rooms}}</span>
+                                    </div>
+                                </div><div class="col-md-3 width-33">
+                                    <div>
+                                        <i class="fa-solid fa-shower icon-size"></i><span class="px-2 font-weight-600">{{$property->bathrooms}}</span>
+                                    </div>
+                                </div><div class="col-md-3 width-33">
+                                    <div>
+                                        <i class="fa-solid fa-vector-square icon-size"></i><span class="px-2 font-weight-600">{{$property->size}} m<sup>2</sup></span>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="properties/{{$property->id}}" class="btn btn-primary">Részletek</a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+</x-layout>
