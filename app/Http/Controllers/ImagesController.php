5b473ea8cc85c44e3b6927db7abac70e7f43fd37
Milan <gebeimilan@gmail.com>
2024-03-15 18:14:42 +0100

    Képek megjelennek

--- app/Http/Controllers/ImagesController.php
+++ app/Http/Controllers/ImagesController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Images;
 use Illuminate\Http\Request;
 
 class ImagesController extends Controller
 {
-
+    public static function img_show($propertyId) {
+        return Images::where('properties_id', '=', $propertyId)->first();
+    }
 }
