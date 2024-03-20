8faa9abeecf4c78b2cf377f5be5d5ea890d56040
Milan <gebeimilan@gmail.com>
2024-03-20 23:15:36 +0100

    Frontend kész (kicsi finomítás kell) + delete backend is kész

--- routes/web.php
+++ routes/web.php
@@ -20,13 +20,26 @@ Route::get('/', function () {
     return view('index');
 });
 
+#Ingatlanok megjelenítése
 Route::get('/properties', [PropertiesController::class, 'index'])->name('properties.index');
 
+#Ingatlanok hozzáadása form megjelenítése
 Route::get('/properties/create', [PropertiesController::class, 'store_index'])->name('properties.create');
+
+#Új ingatlan eltárolása adatbázisban
 Route::post('/properties/store', [PropertiesController::class, 'store'])->name('properties.store');
+
+#Egy ingatlan részletes oldal megjelenítése
 Route::get('/properties/{property}', [PropertiesController::class, 'show']);
 
+#Ingatlanos saját posztjainak megjelenítése
 Route::get('/properties_own', [PropertiesController::class, 'show_own'])->name('properties.own');
+
+#Módosítás form megjelenítése
+Route::get('properties/{property}/edit', [PropertiesController::class, 'edit'])->name('properties.edit');
+
+#Poszt törlése
+Route::delete('properties/{property}', [PropertiesController::class, 'destroy']);
 
 Route::middleware([
     'auth:sanctum',
