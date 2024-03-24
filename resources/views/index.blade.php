190feb835b3bcb3ad25225af143c1646ff2cc9c0
Milan <gebeimilan@gmail.com>
2024-03-24 14:35:53 +0100

    Keresés főoldalon készen van

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,12 @@
 <x-layout>
+    <form action="{{route('properties.search')}}" method="GET">
+        <div class="input-group margin-top">
+            <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" name="search"
+                   aria-describedby="search-addon"/>
+            <button class="btn btn-outline-primary">search</button>
+        </div>
+    </form>
+
     <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
